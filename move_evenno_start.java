694823f8f860b65dd1b64f3940b6a89a98071327
Priya-Coder20 <priyashahu.10th@gmail.com>
2026-01-07 21:13:49 +0530

    Add move even numbers and update zero shifting programs

--- move_evenno_start.java
+++ move_evenno_start.java
@@ -1,29 +1,30 @@
-public class movezero {
-
+public class move_evenno_start {
     public static void main(String[] args) {
-        int[] arr = {10,0,0,20,40,50,60};
+        int [] arr = {3,2,6,13,7,8,20};
         int[] newArr = new int[arr.length];
         rearrange(arr, newArr);
         print(newArr);
-
-
     }
-        
-    
     private static void rearrange(int[] arr, int[] newArr){
         //
-        int j=0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] != 0){
+        int j = 0;
+        for(int i =0; i<arr.length; i++){
+            if(arr[i]%2==0){
                 newArr[j]=arr[i];
                 j++;
             }
         }
+        for(int i =0; i<arr.length; i++){
+            if(arr[i]%2 != 0){
+                newArr[j]= arr[i];
+                j++;
+            }
+        }
     }
-    private static void print(int[] arr) {
+    private static void print(int[] arr){
+        //
         for (int i : arr) {
             System.out.print(i + " ");
         }
     }
-    
 }
